0bb4dbfd6865068fdf01a689d5583611d3e01493
ferreiradv <luancarlosjn@gmail.com>
2021-03-04 16:53:10 -0300

    new files

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,26 +1,78 @@
 const Link = require('../models/Link');
 
-const redirect =  async (req, res) =>{
+const redirect = async (req, res,next) => {
     let title = req.params.title;
-    try{
-        let doc = await Link.findOne({title: title});
-
-        res.redirect(doc.url);
-    }catch(error){
-        res.send('Houver um erro'+error);
+    try {
+        let doc = await Link.findOneAndUpdate({title},{$inc:{click:1}});
+        if(doc){
+            res.redirect(doc.url);
+        }else{
+            next()
+        }
+    } catch (error) {
+        res.send('Houver um erro' + error);
     }
 }
 
 const addLink = async (req, res) => {
     let link = new Link(req.body);
 
-    try{
+    try {
         let doc = await link.save();
-        res.send('Link adicionado com sucesso.');
-    }catch(error){
-        res.render('index',{error,body:req.body})
+        res.redirect('/');
+    } catch (error) {
+        res.render('index', { error, body: req.body })
     }
 
 }
 
-module.exports = {redirect,addLink};
+const allLinks = async (req, res) => {
+    try {
+        let links = await Link.find({});
+        res.render('all',{links});
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+const deleteLink = async (req, res) => {
+    let id = req.params.id
+    try {
+       await Link.findByIdAndDelete(id);
+       res.send(id)
+    } catch (error) {
+        res.status(404).send(error);
+    }
+}
+
+const loadLink = async (req, res) => {
+    let id = req.params.id
+    try {
+       let link = await Link.findById(id);
+       res.render('edit',{error:false,body:link})
+    } catch (error) {
+        res.status(404).send(error);
+    }
+}
+
+const editLink = async (req, res) => {
+    let link = {};
+    link.title = req.body.title;
+    link.description = req.body.description;
+    link.url = req.body.url;
+
+    let id = req.params.id;
+    if(!id){
+        id = req.body.id;
+    }
+
+    try {
+       let doc = await Link.findByIdAndUpdate(id,link);
+       res.redirect('/');
+    } catch (error) {
+        res.render('edit',{error, body:req.body});
+    }
+}
+
+
+module.exports = { redirect, addLink, allLinks, deleteLink,loadLink,editLink }
